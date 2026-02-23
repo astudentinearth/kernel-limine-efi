eb680b5cbe7fb8caf5da69c7d2de94e546fbb3a0
Burak Yeniçeri <burak.yn.dev@gmail.com>
2026-02-23 20:25:13 +0300

    refactor debug code

--- src/debug.c
+++ src/debug.c
@@ -1,5 +1,19 @@
 
 #include "debug.h"
+#include "hardware/serial.h"
+#include "mem.h"
+#include "string.h"
+
+void debug(const char* msg) {
+    debug_puts(msg);
+    outb(COM1, '\n');
+}
+
+void debug_puts(const char* msg) {
+    for(const char* c = msg; *c != 0; c++) {
+        outb(COM1, *c);
+    }
+}
 
 void debug_put_int(int64_t num) {
     char debug_itoa_buffer[DEBUG_ITOA_BUFFER_SIZE];
@@ -22,3 +36,8 @@ void debug_put_hex(uint64_t num) {
     debug_puts("0x");
     debug_puts(debug_itoa_buffer);
 }
+
+void debug_newline() {
+    debug_puts("\n");
+}
+
--- src/include/debug.h
+++ src/include/debug.h
@@ -1,32 +1,21 @@
 #pragma once
-#include "hardware/serial.h"
-#include "mem.h"
 #include "math.h"
 #include "ascii.h"
-#include "string.h"
 
 /**
  * Pushes a debug message into COM1 port. Ends it with a newline.
  */
-static inline void debug(const char* msg) {
-    for(const char* c = msg; *c != 0; c++) {
-        outb(COM1, *c);
-    }
-    outb(COM1, '\n');
-}
+void debug(const char* msg); 
 
 
 /**
  * Pushes a debug message into COM1 port, does not end it with a newline.
  */
-static inline void debug_puts(const char* msg) {
-    for(const char* c = msg; *c != 0; c++) {
-        outb(COM1, *c);
-    }
-}
+void debug_puts(const char* msg);
 
 #define DEBUG_ITOA_BUFFER_SIZE 256
 void debug_put_int(int64_t num);
 void debug_put_uint(uint64_t num);
 void debug_put_hex(uint64_t num);
+void debug_newline();
 
