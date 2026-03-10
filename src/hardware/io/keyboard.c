9fdd4dc035301c3a03b1ecde3b08ca1e74f7e672
Burak Yeniçeri <burak.yn.dev@gmail.com>
2026-03-10 04:27:07 +0300

    very basic kbd layout

--- src/hardware/io/keyboard.c
+++ src/hardware/io/keyboard.c
@@ -14,8 +14,25 @@ static struct io_apic_redirection_entry keyboard_redir_entry = {
     .destination = 0
 };
 
+#define MOD "\0"
+#define US_QWERTY MOD MOD "!@#$%^&*()_+\b\tQWERTYUIOP{}\n" MOD "ASDFGHJKL;'`" MOD "|ZXCVBNM,./" MOD "*" MOD " " 
+#define US_QWERTY_LOWERCASE MOD MOD "1234567890-=\b\tqwertyuiop[]\n" MOD "asdfghjkl:\"`" MOD "\\zxcvbnm<>?" MOD "*" MOD " " 
+char us_qwerty[256] = US_QWERTY;
+char us_qwerty_low[256] = US_QWERTY_LOWERCASE;
+
+bool shift_down = false;
+
 void keyboard_interrupt(uint8_t scancode) {
-    debug_info("KEYPRESS: %d\n", scancode);
+    char ch = shift_down ? us_qwerty[scancode] : us_qwerty_low[scancode];
+    if(scancode == 0x36 || scancode == 0x2A) {
+        shift_down = true;
+    } 
+    else if(scancode == 0xAA || scancode == 0xB6) {
+        shift_down = false;
+    }
+    else if(ch != '\0') {
+        debug_printf("%c", ch);
+    }
     lapic_eoi();
 }
 
