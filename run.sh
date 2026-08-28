825ecc16073f1877b1964400f233952c0cb0c712
Burak <burak.yn.dev@gmail.com>
2026-08-28 23:02:49 +0300

    fix: boot with ovmf

--- run.sh
+++ run.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-qemu-system-x86_64 -m 64M -smp 1 -cdrom image.iso -boot d -serial stdio -no-reboot
+qemu-system-x86_64 \
+    -m 512M \
+    -smp 1 \
+    -cdrom image.iso -boot d \
+    -serial stdio \
+    -no-reboot \
+    -machine q35 -cpu qemu64 \
+    -drive if=pflash,format=raw,unit=0,file=/usr/share/edk2/x64/OVMF_CODE.4m.fd,readonly=on \
+    -drive if=pflash,format=raw,unit=1,file=ovmf_vars.4m.fd \
+    -net none
