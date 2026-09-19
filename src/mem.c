f5ff71c272f65b59ed8f1d6507f483fda2bd8406
Burak <burak.yn.dev@gmail.com>
2026-09-19 16:59:00 +0300

    feat: optimize memcpy with word copying

--- src/mem.c
+++ src/mem.c
@@ -1,14 +1,40 @@
 
 #include "string.h"
 
+typedef u64 copy_word_t __attribute__((__may_alias__, __aligned__(1)));
+
 void *memcpy(void *dest, const void *src, usize n)
 {
-    u8 *pdest = (u8 *)dest;
-    const u8 *psrc = (const u8 *)src;
+    u8 *src_u8 = (u8*)src;
+    u8 *dest_u8 = (u8*)dest;
 
-    for (usize i = 0; i < n; i++) {
-        pdest[i] = psrc[i];
+    // copy until we are word aligned
+    while((uptr)src_u8 % sizeof(copy_word_t) && n > 0) {
+        *dest_u8 = *src_u8;
+        src_u8++;
+        dest_u8++;
+        n--;
     }
+
+    if(n == 0) return dest;
+
+    copy_word_t *src_u64 = (copy_word_t*)src_u8;
+    copy_word_t *dest_u64 = (copy_word_t*)dest_u8;
+
+    // copy words
+    while(n >= sizeof(copy_word_t)) {
+        *dest_u64 = *src_u64;
+        src_u64++;
+        dest_u64++;
+        n -= sizeof(copy_word_t);
+    }
+
+    if(n == 0) return dest;
+
+    src_u8 = (u8*)src_u64;
+    dest_u8 = (u8*)dest_u64;
+
+    for(usize i = 0; i < n; i++) dest_u8[i] = src_u8[i];
 
     return dest;
 }
